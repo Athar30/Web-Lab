44c99ca2f0c5d40a701e2f34bd76e3f1084ba303
123lone <158314907+123lone@users.noreply.github.com>
2025-02-06 23:24:20 +0530

    Delete ML directory

--- ML/main.py
+++
@@ -1,4 +0,0 @@
-import tensorflow;
-print("TensorFlow version:", tf.__version__)
-print("Available devices:", tf.config.list_physical_devices())
-print("GPU available:", tf.config.list_physical_devices('GPU'))
